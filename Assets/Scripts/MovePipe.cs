c950457e3a241ccd3782cd3404f792add3fa82f9
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-05-05 22:12:03 -0500

    Added move pipe script to move pipes on screen

--- Assets/Scripts/MovePipe.cs
+++ Assets/Scripts/MovePipe.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MovePipe : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 0.65f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
     }
 }
