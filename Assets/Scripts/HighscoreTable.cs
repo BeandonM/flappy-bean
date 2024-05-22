2e6fdd808b2669ef366b404b87bf1fc54dc2ee90
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-05-21 19:41:30 -0500

    Added populating the highscores to the highscore table

--- Assets/Scripts/HighscoreTable.cs
+++ Assets/Scripts/HighscoreTable.cs
@@ -8,7 +8,8 @@ public class HighscoreTable : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
-
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
     private void Awake()
     {
         entryContainer = transform.Find("highscoreEntryContainer");
@@ -16,47 +17,74 @@ public class HighscoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        float templateHeight = 45f;
-        for (int i = 1; i <= 10; i++)
+        highscoreEntryList = new List<HighscoreEntry>()
         {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-            entryTransform.gameObject.SetActive(true);
-
-            int rank = i;
-            string rankString;
-            switch (rank)
-            {
-                default:
-                    {
-                        rankString = rank + "TH";
-                        break;
-                    }
-                case 1:
-                    {
-                        rankString = "1ST";
-                        break;
-                    }
-                case 2:
-                    {
-                        rankString = "2ND";
-                        break;
-                    }
-                case 3:
-                    {
-                        rankString = "3RD";
-                        break;
-                    }
-            }
-            entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().text = rankString;
-
-            int score = Random.Range(0, 80);
-            entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
-
-            string name = "Bob";
-            entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().text = name;
-
+            new HighscoreEntry() {score = 10, name = "Bob"},
+            new HighscoreEntry() {score = 11, name = "Bob"},
+            new HighscoreEntry() {score = 12, name = "Bob"},
+            new HighscoreEntry() {score = 13, name = "Bob"},
+            new HighscoreEntry() {score = 14, name = "Bob"},
+            new HighscoreEntry() {score = 15, name = "Bob"},
+            new HighscoreEntry() {score = 16, name = "Bob"},
+            new HighscoreEntry() {score = 17, name = "Bob"},
+            new HighscoreEntry() {score = 18, name = "Bob"},
+            new HighscoreEntry() {score = 19, name = "Bob"},
+        };
+        highscoreEntryTransformList = new List<Transform>();
+        foreach( HighscoreEntry entry in highscoreEntryList)
+        {
+            createHighscoreEntryTransform(entry, entryContainer, highscoreEntryTransformList);
         }
+    }
+    private void createHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList )
+    {
+        float templateHeight = 45f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count;
+        string rankString;
+        switch (rank)
+        {
+            default:
+                {
+                    rankString = rank + "TH";
+                    break;
+                }
+            case 1:
+                {
+                    rankString = "1ST";
+                    break;
+                }
+            case 2:
+                {
+                    rankString = "2ND";
+                    break;
+                }
+            case 3:
+                {
+                    rankString = "3RD";
+                    break;
+                }
+        }
+        entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().text = rankString;
+
+        int score = highscoreEntry.score;
+        entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().text = name;
+
+        transformList.Add(entryTransform);
+    }
+    private class HighscoreEntry
+    {
+        public int id;
+        public string name;
+        public int score;
+        public string score_date;
+        public string score_time;
     }
 }
--- Assets/Scripts/WebManager.cs
+++ Assets/Scripts/WebManager.cs
@@ -1,5 +1,8 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -9,9 +12,20 @@ public class WebManager : MonoBehaviour
     private string url = "https://flappy-bean-backend.onrender.com/api/highscores/addscore";
     private string contentType = "application/json";
 
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            getHighscores();
+        }
+    }
     public void postPlayerScore()
     {
         StartCoroutine(postScore(url));
+    }
+    public void getHighscores()
+    {
+        StartCoroutine(getScore("https://flappy-bean-backend.onrender.com/api/highscores/day"));
     }
     public IEnumerator postScore(string url)
     {
@@ -29,4 +43,39 @@ public class WebManager : MonoBehaviour
             }
         }
     }
+    public IEnumerator getScore(string url)
+    {
+        Debug.Log("Getting Highscores");
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Request Error");
+            }
+            else
+            {
+
+                Debug.Log(request.downloadHandler.text);
+                List<TestData> l = JsonConvert.DeserializeObject<List<TestData>>(request.downloadHandler.text);
+                foreach(TestData test in l)
+                {
+                    Debug.Log(test.name);
+                }
+                //TestData d = JsonConvert.DeserializeObject<TestData>(request.downloadHandler.text);
+                //TestData d = JS.FromJson<TestData>(request.downloadHandler.text);
+                //Debug.Log(d.name);
+            }
+        }
+    }
+}
+
+
+public class TestData
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int score { get; set; }
+    public DateTime score_date { get; set; }
+    public string score_time { get; set; }
 }
